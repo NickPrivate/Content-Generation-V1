bad869f5e80df28ad1967c7b1b726bb4dd5d73b9
NickPrivate <dragonwolf478@gmail.com>
2024-11-09 16:18:23 -0800

    Added support for GPU and CPU rendering

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
 import threading
-import time
-from scripts.scriptgen import generate_script
+
+import torch
+
+from scripts.audiogen import generate_audio
 from scripts.render import render_vid
+from scripts.scriptgen import generate_script
+
 print("Initializing Modlels...")
 from TTS.api import TTS
+
 
 def print_stream(generator):
     script = []
     for word in generator:
-        print(word, end='', flush=True)
+        print(word, end="", flush=True)
         script.append(word)
-    return ''.join(script)
+    return "".join(script)
 
-def generate_audio(script_text, output_file, speaker):
-    tts = TTS(model_name="tts_models/en/vctk/vits", progress_bar=False)
-    tts.to('cuda')
-    tts.tts_to_file(text=script_text, file_path=output_file, speaker=speaker)
 
 def main():
+    device_type = "cuda" if torch.cuda.is_available() else "cpu"
+    if device_type == "cuda":
+        print("GPU Detected, GPU Will be utilized for Faster Rendering")
+    else:
+        print("No GPU Detected, Opting for CPU Rendering")
     word_count = input("Enter word count: ")
     prompt = input("Enter prompt: ")
     our_prompt = f"{prompt} in {word_count} words"
     response_generator = generate_script(our_prompt)
-    
+
     script_text_list = []
 
     def stream_and_capture():
@@ -36,13 +42,11 @@ def main():
     print("\nConverting script to audio...\n")
 
     script_text = script_text_list[0] if script_text_list else ""
-
     output_file = "output.wav"
-
     speaker = "p226"
-    generate_audio(script_text, output_file, speaker)
-    print("\nStream completed. Audio saved to:", output_file)
 
+    generate_audio(script_text, output_file, speaker, device_type)
+    print("\nStream completed. Audio saved to:", output_file)
 
     print("\nNow onto rendering the video")
 
--- scripts/audiogen.py
+++ scripts/audiogen.py
@@ -1,11 +1,21 @@
+import torch
 from TTS.api import TTS
 
-def generate_audio(script_text, output_file):
-    tts = TTS(model_name="tts_models/en/ljspeech/tacotron2-DDC", progress_bar=False, gpu=False)
 
-    tts.tts_to_file(text=script_text, file_path=output_file)
+def generate_audio(script_text, output_file, speaker, devicetype):
+    use_gpu = True if devicetype == "cuda" else False
+    tts = TTS(
+        model_name="tts_models/en/vctk/vits",
+        progress_bar=True,
+        gpu=use_gpu,
+    )
+    tts.to(devicetype)
+    tts.tts_to_file(text=script_text, file_path=output_file, speaker=speaker)
+
 
 if __name__ == "__main__":
     script_text = "Here is a sample script to be converted into speech."
     output_file = "output.wav"
-    generate_audio(script_text, output_file)
+    devicetype = "cuda" if torch.cuda.is_available() else "cpu"
+    speaker = "p226"
+    generate_audio(script_text, output_file, speaker, devicetype)
